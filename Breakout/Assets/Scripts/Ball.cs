543bffbc7219c3d619b9aec4a48cc6da80380e92
ARCade07 <alexandre.rios.cazzaniga@gmail.com>
2024-08-21 16:51:31 -0300

    Ball Movement

--- Breakout/Assets/Scripts/Ball.cs
+++ Breakout/Assets/Scripts/Ball.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    
+    Rigidbody2D rb;
+    [SerializeField] float vel;
+    bool hasLaunched;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        hasLaunched = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey(KeyCode.Space) && !hasLaunched)
+        {
+            rb.velocity = new Vector2(0,vel);
+            hasLaunched = true;
+        }
     }
+
 }
