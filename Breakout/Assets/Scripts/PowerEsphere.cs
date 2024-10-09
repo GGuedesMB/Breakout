73b7b07ef6ff267752945df33840d91fadbacc92
ARCade07 <alexandre.rios.cazzaniga@gmail.com>
2024-10-09 16:56:16 -0300

    Powers implementations

--- Breakout/Assets/Scripts/PowerEsphere.cs
+++ Breakout/Assets/Scripts/PowerEsphere.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PowerEsphere : MonoBehaviour
 {
+    public GameObject shield;
     // Start is called before the first frame update
     void Start()
     {
-        
+        shield = GameObject.Find("Shield");
+        shield.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,6 +23,15 @@ public class PowerEsphere : MonoBehaviour
         {
             int randomPower = Random.Range(0, 10);
             Debug.Log("number: " + randomPower);
+            BiggerPaddle();
         }
+    }
+    private void Shield()
+    {
+        shield.SetActive(true);
+    }
+    private void BiggerPaddle()
+    {
+        FindAnyObjectByType<Player>().gameObject.transform.localScale += new Vector3(3, 0, 0);
     }
 }
